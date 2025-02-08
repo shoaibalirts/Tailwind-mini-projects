b638afb539a87e3f1e16b96873f0021f5aa05a0e
shoaibalirts <shoaibali.rts@gmail.com>
2025-02-08 18:22:13 +0100

    completed Login modal responsive design

--- src/components/loginmodal/LoginModalComp.js
+++ src/components/loginmodal/LoginModalComp.js
@@ -9,21 +9,28 @@ export default function LoginModalComp() {
       <div className="relative flex flex-col m-6 space-y-10 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0 md:m-0">
         <div className="flex flex-row justify-between">
           {/* Left side */}
-          <div>
-            <div className="rounded-xl bg-gray-100">
-              <IoIosClose className="w-10 h-10" />
+          <div className="p-6 md:p-20">
+            <h2 className={`text-xl font-bold`}>Log In</h2>
+            <p>
+              Log in to your account to upload or download pictures, videos or
+              music
+            </p>
+          </div>
+          {/* Right side */}
+          <div className="hidden md:block">
+            <div className="relative h-64 w-[430px]">
+              <Image
+                src="/assets/imagewater.jpg"
+                layout="fill"
+                objectFit="cover"
+                priority
+                alt="beans"
+              />
             </div>
           </div>
-          <div className="relative h-80 w-[400px] md:h-64 md:w-[300px]">
-            <Image
-              src="/assets/imagewater.jpg"
-              layout="fill"
-              objectFit="cover"
-              priority
-              alt="beans"
-              className="rounded-xl md:rounded-l-xl md:rounded-r-none transform hover:scale-105 hover:rounded-xl duration-200"
-            />
-          </div>
+          <button className="absolute top-5 right-4 rounded-xl bg-gray-100 md:top-4">
+            <IoIosClose className="w-10 h-10" />
+          </button>
         </div>
       </div>
     </>
